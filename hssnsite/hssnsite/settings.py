cf8628aafb9f9d908b848d0da4adb7e9bef7d80a
tuyyui <mrjustinyoung991@gmail.com>
2020-11-08 20:13:31 -0600

    Updated Settings.py
    Got rid of personal information

--- hssnsite/hssnsite/settings.py
+++ hssnsite/hssnsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n^ygof8is+7$&t-3k7o9^0dogmd2xkb8_f7^0*j4si2av%#@19'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
